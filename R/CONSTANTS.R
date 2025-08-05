569f295c13d1cc5447842c2fc0df6bd48a0d8b60
Carlos Mora Martínez <carmoma9@gmail.com>
2025-08-05 17:26:47 +0200

    Added constants

---
+++ R/CONSTANTS.R
@@ -0,0 +1,57 @@
+
+# Core
+MULTI_PAGE_PDFS = TRUE
+
+C_CASE = "#FD8B2F" #"rgba(200, 44, 44, 0.8)"
+C_CASE2 = "tomato"
+C_CASE_LINK = "#fBd895" #"#f9c784"
+C_CTRL = "#A1C6EA" # "#006daa" #
+C_CTRL2 = "steelblue2"
+C_CTRL_LINK ="#DAE3E5" #"rgba(44, 44, 200, 0.8)"
+C_CTRL_LINK2 ="#B8C1D4"
+C_WHITE= "#DDDDDD"
+C_NS =  "#A5ABBD" #"rgba(224, 224, 224, 0.8)"
+C_OTHER = "gray30"
+
+## Networks
+C_CASE3 = "#00AA5A"
+C_CTRL3 = "#8E7BFF"
+
+
+mytheme <-  theme_bw()+
+  theme(plot.title = element_text(size = rel(1.5), hjust = 0.5,
+                                  colour = "black", face = "bold")) +
+  theme(legend.title = element_text(face = "bold")) +
+  theme(axis.text.y = element_text(size = 12,
+                                   colour = "black", angle = 0, face = "bold")) +
+  theme(strip.text.y = element_text(size = 12,
+                                    colour = "black", angle = 0, face = "bold")) +
+  theme(strip.text.x = element_text(size = 12,
+                                    colour = "black", angle = 0, face = "bold")) +
+  theme(axis.text.x = element_text(size = 11,
+                                   colour = "black", angle = 0,
+                                   face = "bold"))+
+  theme(axis.title.x = element_text(vjust = 1, hjust = 0.5,
+                                    size = 12, colour = "black",
+                                    angle = 0, face = "bold")) +
+  theme(axis.title.y= element_text(vjust = 1, hjust = 0.5,
+                                   size = 12, colour = "black",
+                                   angle = 90, face = "bold"))
+
+
+
+mystyle <- theme_classic() +
+  theme(axis.text = element_text(face="bold"),
+        axis.title = element_text(face="bold"))
+
+thin_barplot_lines <- theme(panel.grid.major.y = element_line(color = "lightgray",
+                                                              size = 0.05,
+                                                              linetype = 2))
+
+pal1 <- wesanderson::wes_palette("Darjeeling2", 2)
+pal <- wesanderson::wes_palette("AsteroidCity2")
+C_POS_EFF = pal1[2]
+C_NEG_EFF = pal[4]
+
+##
+
